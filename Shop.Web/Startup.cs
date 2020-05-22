2ddad376f24c68748ccaeda3225155413cb12f31
toncristps <toncrist@hotmail.com>
2020-05-21 22:59:54 -0500

    Repository Pattern

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -28,6 +28,8 @@
 
             services.AddTransient<SeedDb>();
 
+            services.AddScoped<IRepository, Repository>();
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
