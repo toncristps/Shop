1247e2fd5a2926994f8c2da23ae9c9072bef117a
toncristps <toncrist@hotmail.com>
2020-05-22 23:57:15 -0500

    Login View Model

---
+++ Shop.UIForms/Shop.UIForms/ViewModels/LoginViewModel.cs
@@ -0,0 +1,57 @@
+﻿namespace Shop.UIForms.ViewModels
+{
+    using GalaSoft.MvvmLight.Command;
+    using System;
+    using System.Windows.Input;
+    using System.Xml;
+    using Xamarin.Forms;
+
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public ICommand LoginCommand => new RelayCommand(this.Login);
+
+        public LoginViewModel()
+        {
+            this.Email = "toncrist@gmail.com";
+            this.Password = "123456";
+        }
+
+
+        private async void Login()
+        {
+            if (String.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Email",
+                    "Accept");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter a Password",
+                    "Accept");
+                return;
+            }
+
+            if (!this.Email.Equals("toncrist@gmail.com") || !this.Password.Equals("123456"))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Incorrect user or password", "Accept");
+                return;
+            }
+
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Fuck Yeah!",
+                    "Accept");
+        }
+    }
+}
